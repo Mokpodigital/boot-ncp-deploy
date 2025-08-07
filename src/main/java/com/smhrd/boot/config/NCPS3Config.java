f4ceee6ca7c463548dc7af80632ac3734dea7105
Mokpodigital <mokpodigital@gmail.com>
2025-08-07 15:34:50 +0900

    first commit

---
+++ src/main/java/com/smhrd/boot/config/NCPS3Config.java
@@ -0,0 +1,40 @@
+package com.smhrd.boot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
+
+@Configuration
+public class NCPS3Config {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
+                .endpointOverride(URI.create("https://kr.object.ncloudstorage.com"))  // 여기에 NCP 엔드포인트 명시
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true)  // 버킷명 path style로 접근하도록 설정
+                        .build())
+                .build();
+    }
+}
